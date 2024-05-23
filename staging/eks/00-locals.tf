6eec366d9e4c2fb0b9978a79912f0281e76276ff
Mike5941 <mike9159@naver.com>
2024-05-23 15:39:12 +0900

    Provision

---
+++ staging/eks/00-locals.tf
@@ -0,0 +1,8 @@
+locals {
+  env         = "staging"
+  region      = "ap-northeast-2"
+  zone1       = "ap-northeast-2a"
+  zone2       = "ap-northeast-2c"
+  eks_name    = "dwitter"
+  eks_version = "1.29"
+}
---
+++ staging/eks/08-nodes.tf
@@ -0,0 +1,73 @@
+resource "aws_iam_role" "nodes" {
+  name = "${local.env}-${local.eks_name}-eks-nodes"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com
+      }
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role   = aws_iam_role.nodes.name
+}
+
+// Pod의 보조 IP를 관리하는 정책
+resource "aws_iam_role_policy_attachment" "amazon_eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_ec2_container_registry_read_only" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role = aws_iam_role.nodes.name
+}
+
+resource "aws_eks_node_group" "general" {
+  cluster_name    = aws_eks_cluster.eks.name
+  version         = local.eks_version
+  node_group_name = "general"
+  node_role_arn   = aws_iam_role.nodes.arn
+
+  subnet_ids = [
+    module.vpc.private_subnets[0]
+  ]
+
+  capacity_type  = "ON_DEMAND"
+  instance_types = ["t3.large"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 10
+    min_size     = 0
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  labels = {
+    role = "general"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.amazon_eks_worker_node_policy,
+    aws_iam_role_policy_attachment.amazon_eks_cni_policy,
+    aws_iam_role_policy_attachment.amazon_ec2_container_registry_read_only,
+  ]
+
+  # desired_capacity 값의 차이가 생겼을 때 에러를 무시
+  lifecycle {
+    ignore_changes = [scaling_config[0].desired_size]
+  }
+}
